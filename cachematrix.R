13d498e67d740ea37dd4c54e40280de71379621a
rcl001 <rcl@kbznet.com>
2019-09-11 18:27:31 -0700

    Initial upload of assignment input.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions create a matrix cache object with functions for
+## managing a cached inverse matrix, calculating the inverse, and
+## retrieving the cached inverse if available.
 
-## Write a short comment describing this function
+## makeCacheMatrix: Accepts a matrix as an input argument and creates
+## an object in the Global Environment with functions for setting and
+## getting the passed matrix and its inverse (computed and stored
+## by a separate function. The passed matrix and its inverse are
+## stored (cached) in the function evaluation environment.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(A = matrix()) {
+        m_inv_cache <- NULL
+        setmat <- function(arg1) {
+                A <<- arg1
+                m_inv_cache <<- NULL
+        }
+        getmat <- function() A
+        setinv <- function(m_inv) m_inv_cache <<- m_inv
+        getinv <- function() m_inv_cache
+        list(setmat = setmat, getmat = getmat,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: Accepts a matrix cache object, created with
+## makeCacheMatrix as its input argument and checks if the 
+## current inverse value is NULL. If its NULL, it computes 
+## the inverse of the cached matrix and stores it in the 
+## object's environment. If the current inverse value is not 
+## NULL it prints a message and returns the cached inverse.
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(CacheObj, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m_inv <- CacheObj$getinv()
+        if(!is.null(m_inv)) {
+                message("getting cached data")
+                return(m_inv)
+        }
+        data <- CacheObj$getmat()
+        m_inv <- solve(data, ...)
+        CacheObj$setinv(m_inv)
+        m_inv
 }
